33f06eff17984304108cbabd2fcf50fcf2a1d0e3
Eric Perennes <eric.perennes@mediane.tm.fr>
2022-03-20 09:53:20 +0100

    Ajoutez des fichiers projet.

---
+++ MigrationDemo/Migrations/202203192250066_AddPostAbstract.cs
@@ -0,0 +1,20 @@
+﻿namespace MigrationDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPostAbstract : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Posts", "Abstract", c => c.String());
+
+            Sql("Update dbo.posts Set Abstract = Left(Content, 100) where Abstract is null ");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Posts", "Abstract");
+        }
+    }
+}
---
+++ MigrationDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MigrationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BlogContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Un autre blog" });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Pressez une touche pour quitter...");
+            Console.ReadKey();
+
+        }
+    }
+}
